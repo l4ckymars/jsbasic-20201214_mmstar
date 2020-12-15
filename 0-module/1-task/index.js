662196b943d5f0a03199af523b78bca1f03ced7f
mmstar <mmstar@inbox.ru>
2020-12-14 20:00:17 -0500

    [на проверку] решение первой задачи

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -5,5 +5,9 @@
  * @returns {number}
  */
 function sum(m, n) {
-  // ваш код...
+  if(m === null || n === null || m === undefined || n === undefined || 
+  	isNaN(m) || isNaN(n) || m.trim() === '' || n.trim() === '') {
+    return 'Одно или оба слагаемых \u2013 не число(а)';
+  }
+  return +m + +n;
 }
