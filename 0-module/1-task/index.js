efa3bdb52207339c6a708bcb2cc6f2f36f29a88e
x <tebya@poime.li>
2020-12-18 19:17:59 -0500

    ещё раз поправил

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -5,10 +5,10 @@
  * @returns {number}
  */
 function sum(m, n) {
-  m = m.replace(/ /g, '');
-  n = n.replace(/ /g, '');
+  m = String(m).replace(/ /g, '');
+  n = String(n).replace(/ /g, '');
   if(Number.isFinite(m) || Number.isFinite(n)) {
       throw new Error('Одно или оба слагаемых \u2013 не число(а)');
   }
-  return m + n;
+  return Number(m) + Number(n);
 }
